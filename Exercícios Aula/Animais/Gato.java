b2c161dd31968ef9ddcc920213cff6ebc8b545f9
Samuel Watthier <98723071+Samuelwt4@users.noreply.github.com>
2025-09-25 19:38:26 -0300

    Create Gato.java

---
+++ Exercícios Aula/Animais/Gato.java
@@ -0,0 +1,11 @@
+package Animais;
+public class Gato extends Animal {
+    public Gato(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println(getNome() + " faz: Miau!");
+    }
+}

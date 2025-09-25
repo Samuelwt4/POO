24ce5e535ac547b08124f8ca4de477ba4cfcd21b
Samuel Watthier <98723071+Samuelwt4@users.noreply.github.com>
2025-09-25 19:25:21 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Animal[] animais = {new Cachorro("rex  "), new Gato("mimi    ")};
-        for(Animal a : animais)
-            a.emitirSom();
-    }
-}
